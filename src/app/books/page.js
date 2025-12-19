3a298531ea30ac497d5e47f5606a2c7e5edc0ca2
Muslim <max2dgame12@gmail.com>
2025-12-19 11:38:55 +0300

    fix

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,14 +1,15 @@
+"use client"
 import BooksComponent from "../components/Books/BooksComponents";
 
 const { default: Footer } = require("../components/App/Footer/Footer");
 const { default: Header } = require("../components/App/Header/Header");
-
+export const dynamic = 'force-dynamic';
 import { Suspense } from "react";
 const Books = () => {
 
     return (
         <div className="flex flex-col !gap-6 !pb-4 !pt-23">
-            <Suspense>
+            <Suspense fallback="<div>Loadin</div>">
 
                 <BooksComponent />
             </Suspense>
