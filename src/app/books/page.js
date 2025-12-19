de6686f6fd4fa1118f4d3911a4d15177eb752fcc
Muslim <max2dgame12@gmail.com>
2025-12-19 11:33:32 +0300

    add suspense

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -3,12 +3,15 @@ import BooksComponent from "../components/Books/BooksComponents";
 const { default: Footer } = require("../components/App/Footer/Footer");
 const { default: Header } = require("../components/App/Header/Header");
 
-
+import { Suspense } from "react";
 const Books = () => {
 
     return (
         <div className="flex flex-col !gap-6 !pb-4 !pt-23">
-            <BooksComponent />
+            <Suspense>
+
+                <BooksComponent />
+            </Suspense>
         </div>
     );
 }
