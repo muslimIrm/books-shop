5057cf1e034b3e97da2a27831ca55c30d6a25ed0
Muslim <max2dgame12@gmail.com>
2025-12-19 14:31:23 +0300

    update next

--- src/app/books/[id]/page.js
+++ src/app/books/[id]/page.js
@@ -1,5 +1,4 @@
 "use client"
-export const runtime = 'edge';
 import { useEffect, useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { useCartContext } from "@/app/context/CartContext";
--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,5 +1,4 @@
 "use client"
-export const runtime = 'edge';
 import { useState, useEffect } from "react";
 import { useCartContext } from "../context/CartContext";
 import Link from "next/link";
