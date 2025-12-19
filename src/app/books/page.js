a5fa5d7df28b59f18ab067b4472788c08dd53360
Muslim <max2dgame12@gmail.com>
2025-12-19 14:41:18 +0300

    bugs

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,16 +1,15 @@
 import BooksComponent from "../components/Books/BooksComponents";
-
-const { default: Footer } = require("../components/App/Footer/Footer");
-const { default: Header } = require("../components/App/Header/Header");
-
-
-const Books = () => {
+import { useSearchParams } from "next/navigation";
+export default function Books({ searchParams }) {
+    const searchParams = useSearchParams().get(("type"))
+    const type =
+        typeof searchParams?.type === "string"
+            ? searchParams.type
+            : "lastBooks";
 
     return (
         <div className="flex flex-col !gap-6 !pb-4 !pt-23">
-            <BooksComponent />
+            <BooksComponent type={type} />
         </div>
     );
 }
-
-export default Books
